64b924fab39d775d435393d1c8907cf013f009b3
Atish <atish6808@gmail.com>
2023-01-04 09:16:50 +0530

    added forcast and completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import {Search} from './components/Search'
+import {Forecast} from './components/forecast'
+import { useState } from 'react';
 import {CurrentWeather} from './components/currentweather'
-import { WEATHER_API_KEY, WEATHER_API_URL } from './components/api';
+let WEATHER_API_URL='https://api.openweathermap.org/data/2.5';
+let WEATHER_API_KEY='';
 function App() {
-  WEATHER_API_URL='https://api.openweathermap.org/data/2.5';
-  WEATHER_API_KEY='';
+  
+  
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange=(searchData)=>{
     const[lat,lon]=searchData.value.split(" ")
@@ -19,13 +24,19 @@ function App() {
       .then(async(response)=>{
         const weatherRes=await response[0].json()
         const forcastRes=await response[1].json()
+        setCurrentWeather({ city: searchData.label, ...weatherRes });
+        setForecast({ city: searchData.label, ...forcastRes });
+
       })
   }
+  console.log(currentWeather)
+  console.log(forecast)
   return (
     
     <div className='container'>
     < Search onSearchChange={handleOnSearchChange}/>
-    <CurrentWeather/>
+    {currentWeather&& <CurrentWeather data={currentWeather}/>}
+    {forecast&&<Forecast data={forecast} />}
     </div>
     
   );
